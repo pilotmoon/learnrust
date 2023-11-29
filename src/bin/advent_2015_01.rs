bed7f6d3fd2a7105362f2b3a3cb7cdeffb52e854
Nick Moore <nick@pilotmoon.com>
2023-11-29 18:06:25 +0000

    fiddling with code

--- src/bin/advent_2015_01.rs
+++ src/bin/advent_2015_01.rs
@@ -2,18 +2,20 @@ use std::io::{self, Read};
 
 fn main() {
     let mut floor = 0;
+    let mut entered_basement = false;
     for (pos, byte) in io::stdin().bytes().enumerate() {
         match byte {
             Ok(b'(') => floor += 1,
             Ok(b')') => floor -= 1,
-            Ok(_) => {}
+            Ok(x) => println!("Unexpected byte: {}", x),
             Err(e) => {
                 eprintln!("Error: {}", e);
                 return;
             }
         }
-        if floor == -1 {
-            println!("Santa entered the basement at position {}", pos + 1);
+        if floor == -1 && !entered_basement {
+            entered_basement = true;
+            println!("Santa first enters the basement at position {}", pos + 1);
         }
     }
     println!("Santa finishes on floor {}", floor);
