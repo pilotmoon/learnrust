ec3532788a4e040dd0df71953d256314343ef10c
Nick Moore <nick@pilotmoon.com>
2023-11-29 17:58:58 +0000

    implement 2015 01 part 2

--- src/bin/advent_2015_01.rs
+++ src/bin/advent_2015_01.rs
@@ -2,7 +2,7 @@ use std::io::{self, Read};
 
 fn main() {
     let mut floor = 0;
-    for byte in io::stdin().bytes() {
+    for (pos, byte) in io::stdin().bytes().enumerate() {
         match byte {
             Ok(b'(') => floor += 1,
             Ok(b')') => floor -= 1,
@@ -11,6 +11,9 @@ fn main() {
                 eprintln!("Error: {}", e);
                 return;
             }
+        }
+        if floor == -1 {
+            println!("Santa entered the basement at position {}", pos + 1);
         }
     }
     println!("Santa finishes on floor {}", floor);
