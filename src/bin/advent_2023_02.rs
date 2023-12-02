aee64339800446c856d0c60abb2da8844f3bc04b
Nick Moore <nick@pilotmoon.com>
2023-12-02 14:02:25 +0000

    simplify trims

--- src/bin/advent_2023_02.rs
+++ src/bin/advent_2023_02.rs
@@ -5,13 +5,16 @@ use std::io;
 fn main() {
     println!("Started");
     let mut sum: usize = 0;
+    let mut sum_power: usize = 0;
     for (i, line) in io::stdin().lines().enumerate() {
         match line {
             Ok(line) => match parse_game(&line) {
                 Ok(game) => {
-                    if possible(game, (12, 13, 14)) {
+                    let (possible, power) = analyse(game, (12, 13, 14));
+                    if possible {
                         sum += i + 1;
                     }
+                    sum_power += power;
                 }
                 Err(e) => {
                     eprintln!("Error parsing game: {}", e);
@@ -23,26 +26,32 @@ fn main() {
             }
         }
     }
-    println!("The final sum is {}", sum);
+    println!(
+        "The sum of possible game id's is {}, sum of powers is {}",
+        sum, sum_power
+    );
 }
 
-fn possible(game: Vec<(usize, usize, usize)>, stock: (usize, usize, usize)) -> bool {
-    let mut max = (0, 0, 0);
+fn analyse(game: Vec<(usize, usize, usize)>, stock: (usize, usize, usize)) -> (bool, usize) {
+    let mut seen = (0, 0, 0);
     for subset in game.iter() {
-        max.0 = cmp::max(max.0, subset.0);
-        max.1 = cmp::max(max.1, subset.1);
-        max.2 = cmp::max(max.2, subset.2);
+        seen.0 = cmp::max(seen.0, subset.0);
+        seen.1 = cmp::max(seen.1, subset.1);
+        seen.2 = cmp::max(seen.2, subset.2);
     }
-    max.0 <= stock.0 && max.1 <= stock.1 && max.2 <= stock.2
+    (
+        seen.0 <= stock.0 && seen.1 <= stock.1 && seen.2 <= stock.2,
+        seen.0 * seen.1 * seen.2,
+    )
 }
 
 fn parse_game(line: &str) -> Result<Vec<(usize, usize, usize)>, String> {
     match line.split_once(':') {
         Some((_, game_str)) => {
             let mut game = Vec::new();
-            for round_str in game_str.split(';').map(|s| s.trim()) {
+            for round_str in game_str.split(';').map(str::trim) {
                 let mut round = (0, 0, 0);
-                for subset_str in round_str.split(',').map(|s| s.trim()) {
+                for subset_str in round_str.split(',').map(str::trim) {
                     match subset_str.split_once(' ') {
                         Some((num, col)) => {
                             let num: Result<usize, _> = num.parse();
@@ -55,13 +64,13 @@ fn parse_game(line: &str) -> Result<Vec<(usize, usize, usize)>, String> {
                                 }
                             }
                         }
-                        None => return Err("Error parsing subset".to_string()),
+                        None => return Err(format!("Error parsing subset {}", subset_str)),
                     }
                 }
                 game.push(round);
             }
             return Ok(game);
         }
-        None => return Err("Error parsing game".to_string()),
+        None => return Err(format!("Error parsing line: {}", line)),
     }
 }
